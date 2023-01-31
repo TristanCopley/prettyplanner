813ee6d0f22c375a1210cb7c91867a8bb16ff298
Tristan Copley <89219797+TristanCopley@users.noreply.github.com>
2023-01-31 15:49:42 -0800

    Slight UI update

--- src/Task.jsx
+++ src/Task.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 const Task = ({time, text}) => {
 
     return (
-        <div className='flex self-center justify-between gap-6 bg-gray-800 w-full'>
+        <div className='flex self-center justify-between gap-6 bg-black w-full px-4'>
             <span className='w-20'>
-                <input type="text" placeholder={time} className='w-20 flex-none text-l py-1 px-2 rounded-sm bg-slate-700 text-white rounded-xm border-none focus:outline-none z-10 text-left'/>  
+                <input type="text" placeholder={time} className='w-24 flex-none text-l py-1 px-2 rounded-sm bg-zinc-900 text-white rounded-xm border-none focus:outline-none z-10 text-left'/>  
             </span>
             <span className='w-full'>
-                <input type="text" placeholder={text} className='w-full flex-none text-l py-1 px-2 rounded-sm bg-slate-700 text-white rounded-xm border-none focus:outline-none z-10 text-right'/>
+                <input type="text" placeholder={text} className='w-full flex-none text-l py-1 px-2 rounded-sm bg-zinc-900 text-white rounded-xm border-none focus:outline-none z-10 text-right'/>
             </span>
-            </div>
+        </div>
     )
 }
 
