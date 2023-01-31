1f6e21ba4c7fd2f91ed0b3d955208c26ec5853eb
Tristan Copley <89219797+TristanCopley@users.noreply.github.com>
2023-01-31 15:49:23 -0800

    Slight UI update

--- src/App.jsx
+++ src/App.jsx
@@ -11,19 +11,24 @@ function App() {
   return (
 
     <div className='App flex flex-col w-full h-screen'>
-      <div className="flex flex-col justify-center flex-wrap gap-2 bg-slate-900 h-full pb-16 p-3 pt-8 relative">
+      <div className="flex flex-col justify-center flex-wrap gap-1 bg-black h-full pb-16 p-3 pt-8 relative">
         {tasks.map((task, index) => {
           return <Task key={index} time={task.time} text={task.text}/>
         })}
       </div>
-      <div className='flex'>
-        <input type="text" placeholder="Time" id='time' className="w-24 h-10 bg-slate-900 text-white text-xl p-3 rounded-none outline outline-white outline-1 outline-offset-[-1px] border-none z-10"/>
-        <input type="text" placeholder="Task" id='text' className="mx-[-1px] w-full h-10 bg-slate-900 text-white text-xl p-3 rounded-none outline-none border-none outline outline-white outline-1 outline-offset-[-1px]"/>
-        <button className="bg-slate-800 text-white text-xl p-3 rounded-none h-10 flex justify-center border-none focus:outline-1 w-24 outline outline-white outline-1 outline-offset-[-1px]" onClick={
-          () => {
-            setTasks([...tasks, {time: document.getElementById('time').value, text: document.getElementById('text').value}])
-          }
-        }><span className='flex self-center text-center'>Add</span></button>
+      <div className='flex justify-around'>
+        <button className="bg-red-800 text-white text-xl p-3 rounded-none h-10 flex justify-center border-none focus:outline-1 w-24 outline outline-white outline-1 outline-offset-[-1px]" onClick={
+            () => {
+              let tasks2 = [...tasks];
+              tasks2.pop();
+              setTasks(tasks2);
+            }
+          }><span className='flex self-center text-center'>-</span></button>
+        <button className="bg-green-800 text-white text-xl p-3 rounded-none h-10 flex justify-center border-none focus:outline-1 w-24 outline outline-white outline-1 outline-offset-[-1px]" onClick={
+            () => {
+              setTasks([...tasks, {time: " ", text: " "}])
+            }
+          }><span className='flex self-center text-center'>+</span></button>
       </div>
     </div>
     
